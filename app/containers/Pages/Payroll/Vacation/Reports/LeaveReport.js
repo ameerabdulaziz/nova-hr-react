51424a2bb557cfbf292040183b10e2b08de4cbfd
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-10-15 13:13:52 +0300

    improve leave reports page with search component

--- app/containers/Pages/Payroll/Vacation/Reports/LeaveReport.js
+++ app/containers/Pages/Payroll/Vacation/Reports/LeaveReport.js
@@ -1,24 +1,19 @@
 import {
-  Autocomplete,
   Backdrop,
   Box,
   Button,
   CircularProgress,
-  Grid,
-  TextField
+  Grid
 } from '@mui/material';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { format } from 'date-fns';
 import { PapperBlock } from 'enl-components';
 import MUIDataTable from 'mui-datatables';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import { useSelector } from 'react-redux';
+import Search from '../../Component/Search';
 import useStyles from '../../Style';
-import GeneralListApis from '../../api/GeneralListApis';
 import payrollMessages from '../../messages';
 import API from '../api/LeaveReportData';
 import messages from '../messages';
@@ -30,10 +25,6 @@ function LeaveReport(props) {
   const locale = useSelector((state) => state.language.locale);
   const [tableData, setTableData] = useState([]);
 
-  const [EmployeeList, setEmployeeList] = useState([]);
-  const [departmentList, setDepartmentList] = useState([]);
-  const [statusList, setStatusList] = useState([]);
-
   const Title = localStorage.getItem('MenuName');
 
   const [formInfo, setFormInfo] = useState({
@@ -42,7 +33,6 @@ function LeaveReport(props) {
     EmployeeId: '',
     EmpStatusId: '',
     OrganizationId: '',
-    branch: [],
   });
 
   const [isLoading, setIsLoading] = useState(false);
@@ -149,23 +139,6 @@ function LeaveReport(props) {
     },
   };
 
-  async function fetchData() {
-    try {
-      const employees = await GeneralListApis(locale).GetEmployeeList();
-      setEmployeeList(employees);
-
-      const department = await GeneralListApis(locale).GetDepartmentList();
-      setDepartmentList(department);
-
-      const status = await GeneralListApis(locale).GetEmpStatusList();
-      setStatusList(status);
-    } catch (error) {
-      toast.error(JSON.stringify(error.response.data));
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
   const formateDate = (date) => format(new Date(date), 'yyyy-MM-dd');
 
   const fetchTableData = async () => {
@@ -212,13 +185,26 @@ function LeaveReport(props) {
   };
 
   useEffect(() => {
-    fetchData();
     fetchTableData();
   }, []);
 
   const onSearchBtnClick = () => {
     fetchTableData();
   };
+
+  const handleChange = useCallback((name, value) => {
+    if (name === 'fromDate') {
+      setFormInfo((prev) => ({ ...prev, FromDate: value }));
+    } else if (name === 'toDate') {
+      setFormInfo((prev) => ({ ...prev, ToDate: value }));
+    } else if (name === 'employeeId') {
+      setFormInfo((prev) => ({ ...prev, EmployeeId: value }));
+    } else if (name === 'organizationId') {
+      setFormInfo((prev) => ({ ...prev, OrganizationId: value }));
+    } else if (name === 'statusId') {
+      setFormInfo((prev) => ({ ...prev, EmpStatusId: value }));
+    }
+  }, []);
 
   return (
     <Box
@@ -241,109 +227,11 @@ function LeaveReport(props) {
         </Backdrop>
 
         <Grid container spacing={3}>
-          <Grid item xs={12} md={3}>
-            <Autocomplete
-              id="departmentId"
-              options={departmentList}
-              getOptionLabel={(option) => option.name}
-              onChange={(_, value) => {
-                setFormInfo((prev) => ({
-                  ...prev,
-                  OrganizationId: value.id,
-                }));
-              }}
-              renderInput={(params) => (
-                <TextField
-                  variant="outlined"
-                  {...params}
-                  name="departmentId"
-                  required
-                  label={intl.formatMessage(messages.department)}
-                />
-              )}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={3}>
-            <Autocomplete
-              id="employeeId"
-              options={EmployeeList}
-              getOptionLabel={(option) => option.name}
-              onChange={(_, value) => {
-                setFormInfo((prev) => ({
-                  ...prev,
-                  EmployeeId: value.id,
-                }));
-              }}
-              renderInput={(params) => (
-                <TextField
-                  variant="outlined"
-                  {...params}
-                  name="employeeId"
-                  required
-                  label={intl.formatMessage(messages.employeeName)}
-                />
-              )}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={3}>
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-              <DesktopDatePicker
-                label={intl.formatMessage(messages.fromdate)}
-                value={formInfo.FromDate}
-                onChange={(date) => {
-                  setFormInfo((prev) => ({
-                    ...prev,
-                    FromDate: date,
-                  }));
-                }}
-                className={classes.field}
-                renderInput={(params) => (
-                  <TextField {...params} variant="outlined" />
-                )}
-              />
-            </LocalizationProvider>
-          </Grid>
-
-          <Grid item xs={12} md={3}>
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-              <DesktopDatePicker
-                label={intl.formatMessage(messages.todate)}
-                value={formInfo.ToDate}
-                onChange={(date) => {
-                  setFormInfo((prev) => ({
-                    ...prev,
-                    ToDate: date,
-                  }));
-                }}
-                className={classes.field}
-                renderInput={(params) => (
-                  <TextField {...params} variant="outlined" />
-                )}
-              />
-            </LocalizationProvider>
-          </Grid>
-
-          <Grid item xs={12} md={3}>
-            <Autocomplete
-              id="StatusList"
-              options={statusList}
-              getOptionLabel={(option) => option.name}
-              onChange={(_, value) => {
-                setFormInfo((prev) => ({
-                  ...prev,
-                  EmpStatusId: value.id,
-                }));
-              }}
-              renderInput={(params) => (
-                <TextField
-                  variant="outlined"
-                  {...params}
-                  name="StatusList"
-                  label={intl.formatMessage(messages.status)}
-                />
-              )}
+          <Grid item xs={12} md={12}>
+            <Search
+              handleChange={handleChange}
+              fromdate={formInfo.FromDate}
+              todate={formInfo.ToDate}
             />
           </Grid>
 
