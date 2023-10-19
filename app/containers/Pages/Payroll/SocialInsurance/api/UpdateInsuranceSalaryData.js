a59811aa3e7e831139f0052501164696d8283fa2
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-10-19 11:55:23 +0300

    finish `UpdateInsuranceSalary` page

---
+++ app/containers/Pages/Payroll/SocialInsurance/api/UpdateInsuranceSalaryData.js
@@ -0,0 +1,21 @@
+import axiosInstance from '../../api/axios';
+const UpdateInsuranceSalaryData = (locale) => {
+  const api = {};
+
+  api.GetInsuMaxLimits = async () => {
+    const data = await axiosInstance.get('SInsuranceEmployee/GetInsuMaxLimits');
+
+    return data.data;
+  };
+
+  api.save = async (params, EmployeeIds) => {
+    const queryString = new URLSearchParams(params);
+    const data = await axiosInstance.post(`SInsuranceEmployee/update?${queryString}`, EmployeeIds);
+
+    return data.data;
+  };
+
+  return api;
+};
+
+export default UpdateInsuranceSalaryData;
