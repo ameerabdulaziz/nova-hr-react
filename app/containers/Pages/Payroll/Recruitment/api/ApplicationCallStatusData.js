5319e2c29a20f0822a2a325de1e2071387f7eb06
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-30 11:13:18 +0200

    create `ApplicationCallStatus` api

---
+++ app/containers/Pages/Payroll/Recruitment/api/ApplicationCallStatusData.js
@@ -0,0 +1,29 @@
+import axiosInstance from '../../api/axios';
+const API = (locale) => {
+  const api = {};
+
+  api.GetList = async () => {
+    const data = await axiosInstance.get(
+      `RecJobApplicationEvaluation/GetCallList/${locale}`
+    );
+
+    const result = data.data;
+
+    return result;
+  };
+
+  api.Save = async (body) => {
+    const data = await axiosInstance.post(
+      `RecJobApplicationEvaluation/SaveCallStatus/${body.appFirstStatus}?CallNote=${body.notes}&interviewTime=${body.interviewTime}`,
+      body.ids
+    );
+
+    const result = data.data;
+
+    return result;
+  };
+
+  return api;
+};
+
+export default API;
