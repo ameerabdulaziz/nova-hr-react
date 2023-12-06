7e1bfbd8c47d3c93515a641c753cf7d48aa588e6
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-12-06 12:12:33 +0200

    replace icons action with dropdown `HRInterviewEvaluation` page

--- app/containers/Pages/Payroll/Recruitment/Transaction/HRInterviewEvaluation.js
+++ app/containers/Pages/Payroll/Recruitment/Transaction/HRInterviewEvaluation.js
@@ -1,6 +1,14 @@
-import EditIcon from '@mui/icons-material/Create';
+import DownloadIcon from '@mui/icons-material/Download';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SystemUpdateAltIcon from '@mui/icons-material/SystemUpdateAlt';
 import UnsubscribeIcon from '@mui/icons-material/Unsubscribe';
-import { Grid } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import {
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem
+} from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import { format } from 'date-fns';
 import notif from 'enl-api/ui/notifMessage';
@@ -14,6 +22,7 @@ import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import PayRollLoader from '../../Component/PayRollLoader';
 import useStyles from '../../Style';
+import { ServerURL } from '../../api/ServerConfig';
 import payrollMessages from '../../messages';
 import api from '../api/HRInterviewEvaluationData';
 import messages from '../messages';
@@ -28,6 +37,8 @@ function HRInterviewEvaluation(props) {
 
   const [tableData, setTableData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const [openedDropdown, setOpenedDropdown] = useState({});
 
   const formateDate = (date) => (date ? format(new Date(date), 'yyyy-MM-dd') : null);
 
@@ -48,13 +59,32 @@ function HRInterviewEvaluation(props) {
     fetchTableData();
   }, []);
 
-  const onUpdateStatusBtnClick = (id) => {
+  const onDropdownClose = (rowIndex) => setOpenedDropdown((prev) => ({
+    ...prev,
+    [rowIndex]: null,
+  }));
+
+  const onUpdateStatusBtnClick = (rowIndex) => {
+    onDropdownClose(rowIndex);
+    const id = tableData[rowIndex]?.id;
+
     history.push('/app/Pages/Recruitment/HRInterviewEvaluationEdit', {
       id,
     });
   };
 
-  const onSendRejectMailBtnClick = async (id) => {
+  const onPreviewCVBtnClick = (rowIndex) => {
+    onDropdownClose(rowIndex);
+    const id = tableData[rowIndex]?.id;
+
+    history.push('/app/Pages/Recruitment/JobApplicationPreview', {
+      id,
+    });
+  };
+
+  const onSendRejectMailBtnClick = async (rowIndex) => {
+    onDropdownClose(rowIndex);
+    const id = tableData[rowIndex]?.id;
     setIsLoading(true);
 
     try {
@@ -112,34 +142,119 @@ function HRInterviewEvaluation(props) {
     },
 
     {
-      name: 'Actions',
-      label: intl.formatMessage(payrollMessages.Actions),
+      name: '',
+      label: '',
       options: {
         filter: false,
-        customBodyRender: (value, tableMeta) => {
+        customBodyRender: (_, tableMeta) => {
           const row = tableData[tableMeta.rowIndex];
-          const isSendMailDisabled = row.mailSend
-            || (row.appFirstStatus !== 2
-            && row.techStatus !== 2
-            && row.secStatus !== 2);
 
           return (
-            <Grid container>
-              <Grid item>
-                <IconButton onClick={() => onUpdateStatusBtnClick(row.id)}>
-                  <EditIcon fontSize='small' />
-                </IconButton>
-              </Grid>
+            <div>
+              <IconButton
+                onClick={(evt) => {
+                  setOpenedDropdown((prev) => ({
+                    ...prev,
+                    [tableMeta.rowIndex]: evt.currentTarget,
+                  }));
+                }}
+              >
+                <MoreVertIcon />
+              </IconButton>
 
-              <Grid item>
-                <IconButton
-                  onClick={() => onSendRejectMailBtnClick(row.id)}
-                  disabled={isSendMailDisabled}
+              <Menu
+                anchorEl={openedDropdown[tableMeta.rowIndex]}
+                open={Boolean(openedDropdown[tableMeta.rowIndex])}
+                onClose={() => onDropdownClose(tableMeta.rowIndex)}
+                slotProps={{
+                  paper: {
+                    elevation: 0,
+                    sx: {
+                      overflow: 'visible',
+                      filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                      mt: 1.5,
+                      '& .MuiAvatar-root': {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                      },
+                      '&:before': {
+                        content: '""',
+                        display: 'block',
+                        position: 'absolute',
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: 'background.paper',
+                        transform: 'translateY(-50%) rotate(45deg)',
+                        zIndex: 0,
+                      },
+                    },
+                  },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+              >
+                <MenuItem
+                  onClick={() => onUpdateStatusBtnClick(tableMeta.rowIndex)}
                 >
-                  <UnsubscribeIcon fontSize='small' />
-                </IconButton>
-              </Grid>
-            </Grid>
+                  <ListItemIcon>
+                    <SystemUpdateAltIcon fontSize='small' />
+                  </ListItemIcon>
+
+                  <ListItemText>
+                    {intl.formatMessage(messages.updateStatus)}
+                  </ListItemText>
+                </MenuItem>
+
+                <MenuItem
+                  onClick={() => onPreviewCVBtnClick(tableMeta.rowIndex)}
+                >
+                  <ListItemIcon>
+                    <VisibilityIcon fontSize='small' />
+                  </ListItemIcon>
+
+                  <ListItemText>
+                    {intl.formatMessage(messages.viewApplicationForm)}
+                  </ListItemText>
+                </MenuItem>
+
+                <MenuItem
+                  component='a'
+                  target='_blank'
+                  disabled={!row.cVfile}
+                  href={ServerURL + 'Doc/CVDoc/' + row.cVfile}
+                  onClick={() => onDropdownClose(tableMeta.rowIndex)}
+                >
+                  <ListItemIcon>
+                    <DownloadIcon fontSize='small' />
+                  </ListItemIcon>
+                  <ListItemText>
+                    {intl.formatMessage(messages.downloadCV)}
+                  </ListItemText>
+                </MenuItem>
+
+                <MenuItem
+                  onClick={() => onSendRejectMailBtnClick(tableMeta.rowIndex)}
+                  disabled={
+                    row.mailSend
+										|| (row.appFirstStatus !== 2
+											&& row.techStatus !== 2
+											&& row.secStatus !== 2)
+                  }
+                >
+                  <ListItemIcon>
+                    <UnsubscribeIcon fontSize='small' />
+                  </ListItemIcon>
+                  <ListItemText>
+                    {row.mailSend && '(sended) '}
+                    {intl.formatMessage(messages.sendRejectMail)}
+                  </ListItemText>
+                </MenuItem>
+              </Menu>
+            </div>
           );
         },
       },
