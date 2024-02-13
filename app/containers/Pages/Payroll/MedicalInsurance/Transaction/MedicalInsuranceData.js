8f4f11ae623512e591736fae67839e24c432018f
mohammed-taysser <mohamedtaysser983@gmail.com>
2024-02-13 17:40:57 +0200

    FIX: Medical Insurance - MedicalInsuranceData - clear minsuranceSubscriptionModel on employee not found

--- app/containers/Pages/Payroll/MedicalInsurance/Transaction/MedicalInsuranceData.js
+++ app/containers/Pages/Payroll/MedicalInsurance/Transaction/MedicalInsuranceData.js
@@ -112,6 +112,7 @@ function MedicalInsuranceData(props) {
       setFormInfo((prev) => ({
         ...prev,
         medInsNotes: '',
+        minsuranceSubscriptionModel: [],
       }));
     } finally {
       setIsLoading(false);
