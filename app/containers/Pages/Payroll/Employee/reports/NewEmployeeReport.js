56c2de87f908587ad4363565c42334a320458878
mohammed-taysser <mohamedtaysser983@gmail.com>
2024-05-02 13:47:57 +0300

    FIX: Employee -  NewEmployeeReport - show employee salary if "showSalary" checked

--- app/containers/Pages/Payroll/Employee/reports/NewEmployeeReport.js
+++ app/containers/Pages/Payroll/Employee/reports/NewEmployeeReport.js
@@ -6,37 +6,37 @@ import {
   Grid,
   TextField,
 } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { format } from 'date-fns';
+import dayjs from 'dayjs';
 import { PapperBlock } from 'enl-components';
-import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import React, { useEffect, useMemo, useState } from 'react';
 import { toast } from 'react-hot-toast';
-import { FormattedMessage, injectIntl } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import { useSelector } from 'react-redux';
+import PayRollLoader from '../../Component/PayRollLoader';
+import PayrollTable from '../../Component/PayrollTable';
 import useStyles from '../../Style';
 import GeneralListApis from '../../api/GeneralListApis';
+import { formateDate } from '../../helpers';
 import payrollMessages from '../../messages';
 import API from '../api/NewEmployeeReportData';
 import messages from '../messages';
-import PayRollLoader from "../../Component/PayRollLoader";
-import { formateDate } from '../../helpers';
-import PayrollTable from '../../Component/PayrollTable';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import dayjs from 'dayjs';
-import Payrollmessages from "../../messages";
 
 function NewEmployeeReport(props) {
   const { intl } = props;
 
   const { classes } = useStyles();
+  const Title = localStorage.getItem('MenuName');
+
   const locale = useSelector((state) => state.language.locale);
   const [tableData, setTableData] = useState([]);
 
   const [departmentList, setDepartmentList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
-  const Title = localStorage.getItem('MenuName');
+  const [dateError, setDateError] = useState({});
 
   const [formInfo, setFormInfo] = useState({
     FromDate: null,
@@ -45,205 +45,147 @@ function NewEmployeeReport(props) {
     ShowSalary: false,
   });
 
-  const [DateError, setDateError] = useState({});
-  
-  // used to reformat date before send it to api
-    const dateFormatFun = (date) => {
-     return  date ? format(new Date(date), "yyyy-MM-dd") : ""
-  }
+  const columns = useMemo(() => {
+    const beforeSalaryColumns = [
+      {
+        name: 'id',
+        options: {
+          filter: false,
+          display: false,
+          print: false,
+        },
+      },
+      {
+        name: 'organizationName',
+        label: intl.formatMessage(messages.organization),
+      },
+      {
+        name: 'employeeId',
+        label: intl.formatMessage(payrollMessages.employeeId),
+        options: {
+          filter: false,
+          display: false,
+          download: false,
+          print: false,
+        },
+      },
 
+      {
+        name: 'employeeCode',
+        label: intl.formatMessage(payrollMessages.employeeCode),
+      },
+      {
+        name: 'employeeName',
+        label: intl.formatMessage(messages.employeename),
+      },
+      {
+        name: 'variableSalary',
+        label: intl.formatMessage(messages.insuranceSalary),
+      },
+      {
+        name: 'mainSalary',
+        label: intl.formatMessage(messages.basicSalary),
+      },
+      {
+        name: 'jobName',
+        label: intl.formatMessage(messages.jopTitle),
+      },
+      {
+        name: 'hiringDate',
+        label: intl.formatMessage(messages.hiringDate),
+      },
+      {
+        name: 'qualification',
+        label: intl.formatMessage(messages.qualification),
+      },
+      {
+        name: 'address',
+        label: intl.formatMessage(messages.address),
+      },
+      {
+        name: 'insuranceDate',
+        label: intl.formatMessage(messages.insuranceDate),
+      },
+      {
+        name: 'taxable',
+        label: intl.formatMessage(messages.taxableStuff),
+      },
+      {
+        name: 'postOverTime',
+        label: intl.formatMessage(messages.postOverTime),
+      },
+    ];
 
+    const afterSalaryColumns = [
+      {
+        name: 'siEmpShareFix',
+        label: intl.formatMessage(messages.insuranceShare),
+      },
+      {
+        name: 'religion',
+        label: intl.formatMessage(messages.religion),
+      },
+      {
+        name: 'qualificationDate',
+        label: intl.formatMessage(messages.qualificationDate),
+      },
+      {
+        name: 'idCardNumber',
+        label: intl.formatMessage(messages.idCardNumber),
+      },
+      {
+        name: 'birthDate',
+        label: intl.formatMessage(messages.birthDate),
+      },
+      {
+        name: 'gender',
+        label: intl.formatMessage(messages.gender),
+      },
+    ];
 
-  const columns = [
-    {
-      name: 'id',
-      options: {
-        filter: false,
-        display: false,
-        print: false,
+    const salaryColumns = [
+      {
+        name: 'netSal',
+        label: intl.formatMessage(messages.netSalary),
       },
-    },
-    {
-      name: 'organizationName',
-      label: intl.formatMessage(messages.organization),
-      options: {
-        filter: true,
+      {
+        name: 'grossSalary',
+        label: intl.formatMessage(messages.grossSalary),
       },
-    },
-    {
-      name: 'employeeId',
-      label: intl.formatMessage(payrollMessages.employeeId),
-      options: {
-        filter: false,
-        display: false,
-        download: false,
-        print: false,
-      },
-    },
+    ];
 
-    {
-      name: 'employeeCode',
-      label: intl.formatMessage(payrollMessages.employeeCode),
-    },
-    {
-      name: 'employeeName',
-      label: intl.formatMessage(messages.employeename),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'variableSalary',
-      label: intl.formatMessage(messages.insuranceSalary),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'mainSalary',
-      label: intl.formatMessage(messages.basicSalary),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'jobName',
-      label: intl.formatMessage(messages.jopTitle),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'hiringDate',
-      label: intl.formatMessage(messages.hiringDate),
-      options: {
-        filter: true,
-        customBodyRender: (value) => (value ? <pre>{formateDate(value)}</pre> : ''),
-      },
-    },
-    {
-      name: 'qualification',
-      label: intl.formatMessage(messages.qualification),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'address',
-      label: intl.formatMessage(messages.address),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'insuranceDate',
-      label: intl.formatMessage(messages.insuranceDate),
-      options: {
-        filter: true,
-        customBodyRender: (value) => (value ? <pre>{formateDate(value)}</pre> : ''),
-      },
-    },
-    {
-      name: 'taxable',
-      label: intl.formatMessage(messages.taxableStuff),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'postOverTime',
-      label: intl.formatMessage(messages.postOverTime),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'netSal',
-      label: intl.formatMessage(messages.netSalary),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'grossSalary',
-      label: intl.formatMessage(messages.grossSalary),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'siEmpShareFix',
-      label: intl.formatMessage(messages.insuranceShare),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'religion',
-      label: intl.formatMessage(messages.religion),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'qualificationDate',
-      label: intl.formatMessage(messages.qualificationDate),
-      options: {
-        filter: true,
-        customBodyRender: (value) => (value ? <pre>{formateDate(value)}</pre> : ''),
-      },
-    },
-    {
-      name: 'idCardNumber',
-      label: intl.formatMessage(messages.idCardNumber),
-      options: {
-        filter: true,
-      },
-    },
-    {
-      name: 'birthDate',
-      label: intl.formatMessage(messages.birthDate),
-      options: {
-        filter: true,
-        customBodyRender: (value) => (value ? <pre>{formateDate(value)}</pre> : ''),
-      },
-    },
-    {
-      name: 'gender',
-      label: intl.formatMessage(messages.gender),
-      options: {
-        filter: true,
-      },
-    },
-  ];
+    if (formInfo.ShowSalary) {
+      return [...beforeSalaryColumns, ...salaryColumns, ...afterSalaryColumns];
+    }
+
+    return [...beforeSalaryColumns, ...afterSalaryColumns];
+  }, [formInfo.ShowSalary]);
 
   async function fetchData() {
     try {
       const department = await GeneralListApis(locale).GetDepartmentList();
       setDepartmentList(department);
     } catch (error) {
-      toast.error(JSON.stringify(error.response.data));
+      //
     } finally {
       setIsLoading(false);
     }
   }
 
   const fetchTableData = async () => {
-
     // used to stop call api if user select wrong date
-    if (Object.values(DateError).includes(true)) {  
-      toast.error(intl.formatMessage(Payrollmessages.DateNotValid));
+    if (Object.values(dateError).includes(true)) {
+      toast.error(intl.formatMessage(payrollMessages.DateNotValid));
       return;
     }
-    
-
 
     try {
       setIsLoading(true);
-      const formData = { ...formInfo };
-
-      formData.FromDate = formateDate(formData.FromDate);
-      formData.ToDate = formateDate(formData.ToDate);
+      const formData = {
+        ...formInfo,
+        FormData: formateDate(formInfo.FromDate),
+        ToDate: formateDate(formInfo.ToDate),
+      };
 
       Object.keys(formData).forEach((key) => {
         formData[key] = formData[key] === null ? '' : formData[key];
@@ -253,7 +195,7 @@ function NewEmployeeReport(props) {
 
       setTableData(dataApi);
     } catch (error) {
-      toast.error(JSON.stringify(error.response.data));
+      //
     } finally {
       setIsLoading(false);
     }
@@ -295,73 +237,69 @@ function NewEmployeeReport(props) {
             />
           </Grid>
 
-                  <Grid item xs={12} md={3}>
-                  
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker 
-                        label={intl.formatMessage(messages.fromDate)}
-                          value={formInfo.FromDate ? dayjs(formInfo.FromDate) : formInfo.FromDate}
-                        className={classes.field}
-                          onChange={(date) => {
-                            setFormInfo((prev) => ({
-                              ...prev,
-                              FromDate: date ,
-                            }))
-                        }}
-                        onError={(error,value)=>{
-                          if(error !== null)
-                          {
-                            setDateError((prevState) => ({
-                                ...prevState,
-                                  [`FromDate`]: true
-                              }))
-                          }
-                          else
-                          {
-                            setDateError((prevState) => ({
-                                ...prevState,
-                                  [`FromDate`]: false
-                              }))
-                          }
-                        }}
-                        />
-                    </LocalizationProvider>
-                  </Grid>
+          <Grid item xs={12} md={3}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label={intl.formatMessage(messages.fromDate)}
+                value={
+                  formInfo.FromDate
+                    ? dayjs(formInfo.FromDate)
+                    : formInfo.FromDate
+                }
+                className={classes.field}
+                onChange={(date) => {
+                  setFormInfo((prev) => ({
+                    ...prev,
+                    FromDate: date,
+                  }));
+                }}
+                onError={(error, value) => {
+                  if (error !== null) {
+                    setDateError((prevState) => ({
+                      ...prevState,
+                      FromDate: true,
+                    }));
+                  } else {
+                    setDateError((prevState) => ({
+                      ...prevState,
+                      FromDate: false,
+                    }));
+                  }
+                }}
+              />
+            </LocalizationProvider>
+          </Grid>
 
-                  <Grid item xs={12} md={3}>
-                  
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker 
-                        label={intl.formatMessage(messages.toDate)}
-                          value={formInfo.ToDate ? dayjs(formInfo.ToDate) : formInfo.ToDate}
-                        className={classes.field}
-                          onChange={(date) => {
-                            setFormInfo((prev) => ({
-                              ...prev,
-                              ToDate: date,
-                            }))
-                        }}
-                        onError={(error,value)=>{
-                          if(error !== null)
-                          {
-                            setDateError((prevState) => ({
-                                ...prevState,
-                                  [`ToDate`]: true
-                              }))
-                          }
-                          else
-                          {
-                            setDateError((prevState) => ({
-                                ...prevState,
-                                  [`ToDate`]: false
-                              }))
-                          }
-                        }}
-                        />
-                    </LocalizationProvider>
-                  </Grid>
-
-
+          <Grid item xs={12} md={3}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label={intl.formatMessage(messages.toDate)}
+                value={
+                  formInfo.ToDate ? dayjs(formInfo.ToDate) : formInfo.ToDate
+                }
+                className={classes.field}
+                onChange={(date) => {
+                  setFormInfo((prev) => ({
+                    ...prev,
+                    ToDate: date,
+                  }));
+                }}
+                onError={(error, value) => {
+                  if (error !== null) {
+                    setDateError((prevState) => ({
+                      ...prevState,
+                      ToDate: true,
+                    }));
+                  } else {
+                    setDateError((prevState) => ({
+                      ...prevState,
+                      ToDate: false,
+                    }));
+                  }
+                }}
+              />
+            </LocalizationProvider>
+          </Grid>
 
           <Grid item md={3}>
             <FormControlLabel
@@ -383,19 +321,19 @@ function NewEmployeeReport(props) {
               color='primary'
               onClick={onSearchBtnClick}
             >
-              <FormattedMessage {...payrollMessages.search} />
+              {intl.formatMessage(payrollMessages.search)}
             </Button>
           </Grid>
         </Grid>
       </PapperBlock>
 
-      <PayrollTable
-        title=''
-        data={tableData}
-        columns={columns}
-      />
+      <PayrollTable title='' data={tableData} columns={columns} />
     </PayRollLoader>
   );
 }
+
+NewEmployeeReport.propTypes = {
+  intl: PropTypes.object.isRequired,
+};
 
 export default injectIntl(NewEmployeeReport);
