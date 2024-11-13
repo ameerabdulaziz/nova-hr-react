54fdae97f9fd6469de1264f0981a19dfd1366012
abdelrahman94545 <abdalrhman.94545@gmail.com>
2024-11-13 17:21:05 +0200

    finish add open month as defualt into Detailed Attendance Report page

--- app/containers/Pages/Payroll/Attendance/Reports/DetailedAttendanceReport.js
+++ app/containers/Pages/Payroll/Attendance/Reports/DetailedAttendanceReport.js
@@ -32,6 +32,7 @@ import PayrollTable from "../../Component/PayrollTable";
 function DetailedAttendanceReport(props) {
   const { intl } = props;
   const locale = useSelector((state) => state.language.locale);
+  const { branchId = null } = useSelector((state) => state.authReducer.user);
   const [data, setdata] = useState([]);
   const [printData, setPrintData] = useState([]);
   const Title = localStorage.getItem("MenuName");
@@ -43,7 +44,7 @@ function DetailedAttendanceReport(props) {
     FromDate: null,
     ToDate: null,
     EmployeeId: "",
-    BranchId: '',
+    BranchId: branchId,
     OrganizationId: "",
     EmpStatusId: 1,
     withoutEmployeesWithoutAttendanceRules: false,
@@ -472,6 +473,59 @@ function DetailedAttendanceReport(props) {
   const reviewDetailsFun =  (type) => {
     handleSearch(printAndReviewType,"review")
   }
+
+
+  const openMonthDateWithCompanyChangeFun = async (BranchId,EmployeeId) => {
+
+  
+
+    let OpenMonthData 
+
+    try
+    {
+      if(!EmployeeId)
+      {
+         OpenMonthData = await GeneralListApis(locale).getOpenMonth( BranchId,0);
+      }
+      else
+      {
+         OpenMonthData = await GeneralListApis(locale).getOpenMonth( 0,EmployeeId);
+      }
+
+      
+      setsearchData((prev)=>({
+        ...prev,
+        FromDate: OpenMonthData ? OpenMonthData.fromDateAtt : null,
+        ToDate: OpenMonthData ? OpenMonthData.todateAtt : null,
+      }))
+    }
+    catch(err)
+    {}
+
+  }
+
+
+  useEffect(()=>{
+    if(searchData.BranchId)
+    {      
+      openMonthDateWithCompanyChangeFun(searchData.BranchId)
+    }
+
+    if(searchData.BranchId === "" && searchData.EmployeeId !== "")
+    {
+      openMonthDateWithCompanyChangeFun(0, searchData.EmployeeId)
+    }
+
+    if(searchData.BranchId === "" && searchData.EmployeeId === "")
+    {
+      setsearchData((prev)=>({
+        ...prev,
+        FromDate: null,
+        ToDate: null,
+      }))
+    }
+
+  },[searchData.BranchId, searchData.EmployeeId])
 
 
   return (
