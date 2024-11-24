dbe20129db1f4543b3f17a7c3757e1ca469e32d4
abdelrahman94545 <abdalrhman.94545@gmail.com>
2024-11-24 12:29:41 +0200

    finish add filter by company into Employee list page

--- app/containers/Pages/Payroll/Employee/Code/EmployeeList.js
+++ app/containers/Pages/Payroll/Employee/Code/EmployeeList.js
@@ -1,28 +1,51 @@
-import { Box, Button } from '@mui/material';
+import { Box, Button, Autocomplete, Grid, TextField } from '@mui/material';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
-import { injectIntl } from 'react-intl';
+import { injectIntl, FormattedMessage } from 'react-intl';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import PayrollTable from '../../Component/PayrollTable';
 import { formateDate, getCheckboxIcon } from '../../helpers';
 import ApiData from '../api/PersonalData';
 import messages from '../messages';
+import payrollMessages from "../../messages";
 import EmployeeNavigation from '../../Component/EmployeeNavigation';
 import Payrollmessages from "../../messages";
+import { PapperBlock } from "enl-components";
+import PayRollLoader from "../../Component/PayRollLoader";
+import GeneralListApis from "../../api/GeneralListApis";
+import { getAutoCompleteValue } from '../../helpers';
 
 function EmployeeList(props) {
   const { intl } = props;
   const history = useHistory();
   const locale = useSelector((state) => state.language.locale);
+    const { branchId = null } = useSelector((state) => state.authReducer.user);
   const [data, setdata] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const Title = localStorage.getItem('MenuName');
+  const [companyList, setCompanyList] = useState([]);
+  const [filterHighlights, setFilterHighlights] = useState([]);
+  const [searchData, setsearchData] = useState({
+    BranchId: branchId,
+  });
 
   async function fetchData() {
     try {
-      const dataApi = await ApiData(locale).GetList();
+      let formData = {
+        BranchId: searchData.BranchId
+      };
+      Object.keys(formData).forEach((key) => {
+        formData[key] = formData[key] === null ? "" : formData[key];
+      });
+
+      const dataApi = await ApiData(locale).GetList(formData);
       setdata(dataApi);
+
+      const company = await GeneralListApis(locale).GetBranchList();
+      setCompanyList(company);
+
+      getFilterHighlights();
     } catch (err) {
       //
     } finally {
@@ -45,6 +68,25 @@ function EmployeeList(props) {
       setIsLoading(false);
     }
   }
+
+
+  const getFilterHighlights = () => {
+    const highlights = [];
+
+    const company = getAutoCompleteValue(companyList, formInfo.BranchId);
+
+    if (company) {
+      highlights.push({
+        label: intl.formatMessage(messages.company),
+        value: company.name,
+      });
+    }
+
+    setFilterHighlights(highlights);
+  };
+
+
+
   const columns = [
     {
       name: 'id',
@@ -223,7 +265,78 @@ function EmployeeList(props) {
     ),
   };
 
+
+
+  const handleSearch = async (e) => {
+
+        try {
+          setIsLoading(true);
+
+          let formData = {
+            BranchId: searchData.BranchId
+          };
+          Object.keys(formData).forEach((key) => {
+            formData[key] = formData[key] === null ? "" : formData[key];
+          });
+          
+          const dataApi = await ApiData(locale).GetList(formData);
+          setdata(dataApi);
+
+        } catch (err) {
+          
+        } finally {
+          setIsLoading(false);
+        }
+   
+  };
+
+  const onAutoCompleteChange = (value, name) => {
+    setsearchData((prev) => ({
+      ...prev,
+      [name]: value  ? value.id : "",
+    }));
+  };
+
   return (
+    <PayRollLoader isLoading={isLoading}>
+      <PapperBlock whiteBg icon="border_color" title={Title} desc="">
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={3}>
+              <Autocomplete
+                options={companyList}
+                value={searchData.BranchId ? companyList.find(item => item.id === searchData.BranchId) ?? null : null}
+                isOptionEqualToValue={(option, value) => option.id === value.id
+                }
+                getOptionLabel={(option) => (option ? option.name : '')}
+                renderOption={(propsOption, option) => (
+                  <li {...propsOption} key={option.id}>
+                    {option.name}
+                  </li>
+                )}
+                onChange={(e, value) => onAutoCompleteChange(value ? value : "", 'BranchId')
+                }
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label={intl.formatMessage(Payrollmessages.company)}
+                  />
+                )}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={2}>
+              <Button
+                variant="contained"
+                size="medium"
+                color="primary"
+                onClick={handleSearch}
+              >
+                <FormattedMessage {...payrollMessages.search} />
+              </Button>
+          </Grid>
+        </Grid>
+      </PapperBlock>
+
     <PayrollTable
       isLoading={isLoading}
       showLoader
@@ -231,7 +344,9 @@ function EmployeeList(props) {
       data={data}
       columns={columns}
       actions={actions}
+      filterHighlights={filterHighlights}
     />
+    </PayRollLoader>
   );
 }
 
--- app/containers/Pages/Payroll/Employee/api/PersonalData.js
+++ app/containers/Pages/Payroll/Employee/api/PersonalData.js
@@ -2,11 +2,13 @@ import axiosInstance from '../../api/axios';
 const PersonalData = (locale) => {
   const Apis = {};
 
-  Apis.GetList = async () => {
-    const data = await axiosInstance.get(`EmpEmployee/AllData/${locale}`);
+  Apis.GetList = async (params) => {
+    const queryString = new URLSearchParams(params);
+    const data = await axiosInstance.get(`EmpEmployee/AllData/${locale}?${queryString}`);
     const result = data.data;
     return result;
   };
+
   Apis.Delete = async (Item) => {
     const data = await axiosInstance.delete(`EmpEmployee/${Item}`);
     const result = data.data;
